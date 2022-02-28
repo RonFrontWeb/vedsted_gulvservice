126bcaf3f6b53dc3ae926fbfcd4307df08674b90
RonFrontWeb <62059074+RonFrontWeb@users.noreply.github.com>
2022-02-28 13:06:33 +0100

    final touch

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
  var burger = document.querySelector(".navbar__burgermenu")
+ var cta = document.querySelector(".hero__btn")
+
 
  burger.addEventListener("click",function (evt) {
      document.querySelector(".navbar__links").classList.toggle("navbar__links--toggle")
+ })
+
+ cta.addEventListener("click",function (evt) {
+     alert("Looking forward to working with you :) ")
  })
